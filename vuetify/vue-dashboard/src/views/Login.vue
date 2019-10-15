b33aa3fdab7ba6db7734a2bbb05026a032042ba4
Bogdan Petre <petre.bg@gmail.com>
2019-10-15 15:01:23 +0300

    vuetify components 1-2

---
+++ vuetify/vue-dashboard/src/views/Login.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-card width="500" class="mx-auto mt-5">
+    <v-card-title>
+      <h1 class="display-1">Login</h1>
+    </v-card-title>
+    <v-card-text>
+      <v-form>
+        <v-text-field label="Username" prepend-icon="mdi-account-circle" />
+        <v-text-field
+          :type="showPassword ? 'text' : 'password'"
+          label="Password"
+          prepend-icon="mdi-lock"
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          @click:append="showPassword = !showPassword"
+        />
+      </v-form>
+    </v-card-text>
+    <v-divider></v-divider>
+    <v-card-actions>
+      <v-btn color="success" text rounded>Register</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn color="info" text rounded>Login</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "LoginPage",
+  data() {
+    return {
+      showPassword: false
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
